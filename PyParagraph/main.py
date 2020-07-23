b04deae050095ad255924b3ccc45e8cbc9d74148
rick <rickhaugan@msn.com>
2020-07-23 00:49:51 -0500

    Update main.py

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -1,67 +1,67 @@
 import os
 import re
+from nltk.tokenize import sent_tokenize
 
 #gets current path
 mypath = os.getcwd()
 #adds current path to file source path
-filename = input("Enter the filename you want to process. (paragraph_1.txt,paragraph_2.txt or paragraph_3.txt):")
+filename = input("Enter the filename you want to process. (Default:paragraph_3.txt) --->")
+
+#Default filename if nothing entered
+if filename == "":
+    filename = "paragraph_3.txt"
+
 mypathfile1 = mypath + "\\raw_data\\" + filename
-#open file
-file1 = open(mypathfile1,"r") 
-#split out file by words
-filedata = (str(file1.readlines()).split())
-file1.close()
 
-totalletterCount = 0
-averagelettercountperfile = 0
-for x in filedata:
-    totalletterCount += int(len(x))
-
-#Approx Average letter count
-averagelettercountperfile =  round(totalletterCount / int(len(filedata)),2)
-
-
-#File read without splitting
+#File read 
 filesub = open(mypathfile1,"r") 
-filedataNOSPLIT = (filesub.readlines())
+filedata = (filesub.readlines())
 filesub.close()
 
+
 entireFilePerLine = []
-for line in filedataNOSPLIT:
+for line in filedata:
     entireFilePerLine.append(line)
-   
-# print(f"entire file per line var {len(entireFilePerLine)}")
+
+
+#split file into words
+filedatatosplit = []
+filedatatosplit = str(filedata).split()
+
+
+totallettercount = 0
+#get total letters per all words in file
+for words in filedatatosplit:
+    totallettercount += int(len(re.sub("[^A-Za-z0-9_]","",words)))
+
+
+#average of total letters per word per file
+avglettersperwordperfile = round(totallettercount / int(len(filedatatosplit)),2)
+
+
+# split file by sentence
 mytextlistbysentence = []
-mytextlistbysentence = re.split("(?<=[.!?]) +", entireFilePerLine[0], maxsplit=0)
+for line in entireFilePerLine:
+    #imported package https://www.nltk.org/
+    mytextlistbysentence += (sent_tokenize(str(line))) 
 
 
-# for words in mytextlist:
-mysentencesplit = []
+
+# gets words in sentence
+# gets sentence length
+# sums sentences lengths
+wordsinsent = []
+sentencelength = []
+sumofsentenceslength = 0
 for sentence in mytextlistbysentence:
-    mysentencesplit.append(sentence.split())
+    sentencelength.append(str(len(sentence)))
+    wordsinsent +=(str(sentence).split())
+    sumofsentenceslength += len(sentence)
 
-wordcounttotal = 0
-wordcountpersentence = []
-#split sentences into word count
-for x in mysentencesplit:
-    wordcountpersentence.append(len(str(x).split()))
+avglengthpersent = round(sumofsentenceslength/len(sentencelength),2)
+sentence_count = len(mytextlistbysentence)
 
-lettercountperword = []
-totalwords = 0
-for x in mysentencesplit:
-    lettercountperword.append(len(str(x).split()))
-    totalwords += len(str(x).split())
-
-#count words in sentences
-for x in wordcountpersentence:
-    wordcounttotal += int(x)
-
-
-
-sentence_count = len(mysentencesplit)
-avgwordspersent = wordcounttotal / sentence_count
-
-print(f"Approximate Word Count: {totalwords}")
-print (f"Approximate Sentence Count: {sentence_count}")
-print(f"Average Letter Count: {averagelettercountperfile}")
-print (f"Average Sentence Length: {avgwordspersent}")
+print(f"Approximate Word Count: {len(wordsinsent)}")
+print(f"Approximate Sentence Count: {sentence_count}")
+print(f"Average Letter Count: {avglettersperwordperfile}")
+print(f"Average Sentence Length: {avglengthpersent}")
